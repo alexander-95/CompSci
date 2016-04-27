666dcd52dc25c1c9f0297bdee87fb7d96fe84015
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2016-04-27 16:58:15 +0100

    implemented stack

--- stack.c
+++ stack.c
@@ -5,14 +5,37 @@ struct stack
     int data[10]; //array
 };
 
+void initialiseStack(struct stack* S)
+{
+	S->sp = 0;
+	int i = 0;
+    for(i=0;i<10;i++)S->data[i]=0;
+}
+
+void printStack(struct stack* S)
+{
+	int i = 0;
+	for(i=0;i<S->sp;i++)printf("%d\n",S->data[i]);
+	printf("sp = %d\n",S->sp);
+}
+
+void push(struct stack* S, int n)
+{
+	if(S->sp<10)S->data[S->sp++] = n;
+	else printf("Shaq Ogreflow\n");
+}
+
+void pop(struct stack* S)
+{
+	if(S->sp<10)S->data[S->sp--] = 0;
+}
+
 int main()
 {
-    struct stack s;
-    s.sp=10;
-    int i=0;
-    for(i=0;i<10;i++)s.data[i]=i*2;
-    printf("hello\n");
-    printf("%d\n",s.sp);
-    for(i=0;i<10;i++)printf("%d\n",s.data[i]);
-    return 0;
+	int i = 0;
+	struct stack s;
+	initialiseStack(&s);
+	for(i=0;i<5;i++)push(&s, i*5);
+    printStack(&s);
+	return 0;
 }
