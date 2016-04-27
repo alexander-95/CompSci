3bbd629b90984530ef96882e62f10ec04f23a792
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2016-04-27 12:58:51 +0100

    started stack implementation

--- stack.c
+++ stack.c
@@ -7,5 +7,12 @@ struct stack
 
 int main()
 {
+    struct stack s;
+    s.sp=10;
+    int i=0;
+    for(i=0;i<10;i++)s.data[i]=i*2;
+    printf("hello\n");
+    printf("%d\n",s.sp);
+    for(i=0;i<10;i++)printf("%d\n",s.data[i]);
     return 0;
 }
